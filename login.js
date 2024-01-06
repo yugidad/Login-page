7022fdfb22a62c7c796bbab85446f58200b926ed
GIDADO YUSUF OLATUNJI <118447666+yugidad@users.noreply.github.com>
2024-01-06 23:34:44 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+// const container = document.querySelector(".container");
+// const clickMain = document.querySelector(".click1");
+// const clickAgain = document.querySelector(".click2");
+
+
+// clickMain.addEventListener("click", () => {
+//     container.classList.add("active");
+// });
+// clickAgain.addEventListener("click", () => {
+//     container.classList.remove("active");
+// });
+
+for(let i = 0; i <= 100, i++;){
+    console.log( `${i}, sorry Babu \n`);
+};
