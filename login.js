f7d6c93bf9e214f40ba9ba3f2210b605d4dfa201
GIDADO YUSUF OLATUNJI <118447666+yugidad@users.noreply.github.com>
2024-01-08 10:38:55 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,11 @@
-// const container = document.querySelector(".container");
-// const clickMain = document.querySelector(".click1");
-// const clickAgain = document.querySelector(".click2");
+const container = document.querySelector(".container");
+const clickMain = document.querySelector(".click1");
+const clickAgain = document.querySelector(".click2");
 
 
-// clickMain.addEventListener("click", () => {
-//     container.classList.add("active");
-// });
-// clickAgain.addEventListener("click", () => {
-//     container.classList.remove("active");
-// });
-
-for(let i = 0; i <= 100, i++;){
-    console.log( `${i}, sorry Babu \n`);
-};
+clickMain.addEventListener("click", () => {
+    container.classList.add("active");
+});
+clickAgain.addEventListener("click", () => {
+    container.classList.remove("active");
+});
